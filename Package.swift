0775de6f1bdfb1b0b1e0e4c60cef58e4f6b88012
Sergej Jaskiewicz <seergy_pro@me.com>
2017-06-18 20:52:59 +0300

    Revert "Extract CLibPNG to a system package"
    This reverts commit c544d61d84b3e6fadc978f38b69f84217f7755e2.

--- Package.swift
+++ Package.swift
@@ -5,12 +5,11 @@ import PackageDescription
 let package = Package(
     name: "SwiftyHaru",
     targets: [
-        Target(name: "CLibHaru"),
+        Target(name: "CLibPNG", dependencies: []),
+        Target(name: "CLibHaru", dependencies: ["CLibPNG"]),
         Target(name: "SwiftyHaru", dependencies: ["CLibHaru"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/jessesquires/DefaultStringConvertible.git", majorVersion: 2),
-        .Package(url: "https://github.com/WeirdMath/CZlib.git", majorVersion: 0),
-        .Package(url: "https://github.com/WeirdMath/CLibPNG.git", majorVersion: 0)
+        .Package(url: "https://github.com/jessesquires/DefaultStringConvertible.git", majorVersion: 2)
     ]
 )
